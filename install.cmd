979063d2aebd38ad4bba2748473b0a4e16d1c07b
Artoria <Artoria>
2013-04-14 16:39:33 +0800

    Desktop.ini

--- install.cmd
+++ install.cmd
@@ -2,5 +2,14 @@
 setlocal
 pushd %~dp0
 cscript /nologo Side\pre\runner.vbs install.vbs
+pushd %userprofile%\SendTo\Side
+attrib /d /s +r .
+attrib /d /s +r .\*
+attrib +h icon
+attrib +h pre
+attrib +h src
+attrib +h common
+attrib /s +r +h +s Desktop.ini
+popd
 gpupdate /force /wait:0
 endlocal
